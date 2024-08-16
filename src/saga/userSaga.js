85cbc04e7197224644208a6a6a0c0e6aa7e2e84c
Исса <108079241+kodzoevIssa@users.noreply.github.com>
2024-08-16 20:56:18 +0300

    first commit

--- src/saga/userSaga.js
+++ src/saga/userSaga.js
@@ -5,7 +5,9 @@ const fetchUsersFromApi = () =>
     fetch('https://jsonplaceholder.typicode.com/users?_limit=10');
 
 function* fetchUsersWorker() {
-    
+    const data = yield call(fetchUsersFromApi);
+    const json = yield call(() => new Promise((res) => res(data.json())));
+    yield put(setUsers(json));
 }
 
 export function* fetchUsersWatcher() {
